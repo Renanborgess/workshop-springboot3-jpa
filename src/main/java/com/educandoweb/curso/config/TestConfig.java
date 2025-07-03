d78311dd6097a329f45022f8b2e31dc397471fc6
Renanborgess <renanborgessti@gmail.com>
2025-07-02 22:35:23 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -51,6 +51,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null, "Maiara Schvambach", "maiaraas12@hotmail.com", "996739172", "789456123");
 		User u2 = new User(null, "Tufa Borges", "tufa@nenem.dog", "9554521455", "124563129");
 		
